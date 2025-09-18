555f6b075a776c9b523780767c2d74944ca4a235
Ashton Fairhurst <fai21013@byui.edu>
2025-09-18 15:24:42 -0600

    Finnished the assignments for learning activity prep 1 and 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What grade did you get in class? ");
+        string userInput = Console.ReadLine();
+        int grade = int.Parse(userInput);
+        string LetterGrade;
+        if (grade >= 90)
+        {
+            LetterGrade = "A";
+        }
+        else if (grade < 90 && grade >= 80)
+        {
+            LetterGrade = "B";
+        }
+        else if (grade < 80 && grade >= 70)
+        {
+            LetterGrade = "C";
+        }
+        else if (grade < 70 && grade >= 60)
+        {
+            LetterGrade = "D";
+        }
+        else
+        {
+            LetterGrade = "F";
+        }
+        if (LetterGrade == "A" || LetterGrade == "F")
+        {
+            Console.WriteLine($"You got an {LetterGrade}");
+        }
+        else
+        {
+            Console.WriteLine($"You got a {LetterGrade}");
+        }
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congradulations, you passed the class");
+        }
+        else
+        {
+            Console.WriteLine("Unfortunately, you did not pass the class");
+        }
+
     }
 }
