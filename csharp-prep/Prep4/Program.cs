439afae1a550cac01321c2ae3ae5a4407310c5f4
Ashton Fairhurst <fai21013@byui.edu>
2025-09-25 16:28:36 -0600

    Turning in the learning activity for prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,38 @@
 using System;
+using System.Transactions;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finnished.");
+        int UserNumber = 1;
+        List<int> NumberList = new List<int>();
+        int ListCount = 0;
+        int ListSum = 0;
+        int LargestNumber = 0;
+        while (UserNumber != 0)
+        {
+            Console.Write("Enter number: ");
+            UserNumber = int.Parse(Console.ReadLine());
+            if (UserNumber != 0)
+            {
+                NumberList.Add(UserNumber);
+                ListCount = ListCount + 1;
+            }
+
+        }
+        foreach (int number in NumberList)
+        {
+            ListSum = ListSum + number;
+            if (number > LargestNumber)
+            {
+                LargestNumber = number;
+            }
+        }
+        float ListAverage = ListSum / (float)ListCount;
+        Console.WriteLine($"The sum is: {ListSum}");
+        Console.WriteLine($"The average is: {ListAverage}");
+        Console.WriteLine($"The largest number is: {LargestNumber}");
     }
 }
