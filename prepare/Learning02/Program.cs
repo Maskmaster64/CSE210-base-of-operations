0835f9b518b8faf40d36210120ec0be6a75020e9
Ashton Fairhurst <fai21013@byui.edu>
2025-10-02 14:54:47 -0600

    This is for the W3 prepare learning activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._companyName = "Apple";
+        job1._jobTitle = "Manager";
+        job1._startYear = 2022;
+        job1._endYear = 2023;
+
+        Job job2 = new Job();
+        job2._companyName = "Microsoft";
+        job2._jobTitle = "Software Engineer";
+        job2._startYear = 2019;
+        job2._endYear = 2022;
+
+        Resume myResume = new Resume();
+        myResume._name = "Ashton";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.DisplayResume();
     }
 }
