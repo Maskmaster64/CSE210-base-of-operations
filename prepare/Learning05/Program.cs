7724e34ea6e23ad0989c3b21b28b8e5976098956
Ashton Fairhurst <fai21013@byui.edu>
2025-11-13 15:01:42 -0700

    This is the program for the W09 learning activity.

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,29 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Shape square = new Square(15.3, "Yellow");
+
+        Shape rectangle = new Rectangle(4.3, 7.9, "Blue");
+
+        Shape circle = new Circle(11.45, "Green");
+
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+            Console.WriteLine($"The {color} shape has an area of {area} units.");
+        }
+
+
     }
 }
